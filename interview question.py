4f8798579d7f51b4f2106a7293ae062381898d63
chhaya742 <76725368+chhaya742@users.noreply.github.com>
2021-03-14 09:48:09 +0530

    Add files via upload

---
+++ interview question.py
@@ -0,0 +1,64 @@
+# d ={ 
+#   "fantasy": "harrypotter", 
+#   "romance": "me before you", 
+#   "fiction": "divergent"
+#   } 
+
+# print(d.items()) 
+
+# for i in d.iteritems():
+    # print(i)
+
+# dict1={1:['Samuel', 21, 'Data Structures','ssism'],
+#        2: ['Richie', 20,'Machine Learning','ssism'],
+#        3: ['Lauren', 21, 'OOPS with java','ssism']
+#        } 
+# print ("{:<10} {:<10} {:<10}         {:<10}".format('NAME', 'AGE', 'COURSE','Collage')) 
+# for key, value in dict1.items(): 
+#     name, age, course , collage = value 
+#     print ("{:<10} {:<10} {:<10}     {:<10}".format(name, age, course, collage)) 
+
+
+# print('NAME','     ''AGE', '        '  'COURCE',  '              ' 'COLLAGE')
+  
+# for key, value in dict1.items(): 
+#     name, age, course , collage = value 
+#     print (name, '  '  ,age, '      '   ,course,'    ', collage)
+
+
+# dict1 = {'NAME':['Samuel', 'Richie', 'Lauren'], 
+#          'AGE':[21, 20, 21], 
+#          'COURSE':['Data Structures', 'Machine Learning', 'OOPS with Java']} 
+  
+# print the contents using zip format. 
+# for each_row in zip(*([i] + (j)  
+#                       for i, j in dict1.items())): 
+      
+#       print(*each_row, " ") 
+
+
+
+
+
+# dict1={(0,0):'Samuel',(0,1):21,(0,2):'Data Structures',
+#         (1,0): 'Richie', (1,1):20,(1,2):'Machine Learning',
+#         (2,0): 'Lauren',(2,1): 21,(2,2):'OOPS with java'
+#         } 
+# print('NAME','  ','AGE','  ','COURSE')
+# for i in range(3):
+#     for j in range(3):
+        # print(dict1[i,j,end=' ')
+        
+# print(dict1['NAME'])
+
+
+
+
+ 
+
+
+
+ 
+
+
+
